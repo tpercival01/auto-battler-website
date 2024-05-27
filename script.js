e4503b5459b845d592e9b583b45e0d217dfbe351
Thomas Percival <thomaspercival@live.co.uk>
2024-05-27 15:24:56 +0100

    Added new framework

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
     preload: preload,
     create: create,
@@ -9,10 +16,33 @@ var config = {
   },
 };
 
+var platforms;
+
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image(
+    "sky",
+    "Assets/craftpix-net-481981-free-summer-pixel-art-backgrounds/PNG/summer 2/1.png"
+  );
+  this.load.image(
+    "ground",
+    "Assets/craftpix-net-481981-free-summer-pixel-art-backgrounds/PNG/summer 2/2.png"
+  );
+  this.load.spritesheet(
+    "wizard1",
+    "Assets/craftpix-net-431295-free-wizard-sprite-sheets-pixel-art/Lightning Mage/Idle.png",
+    { frameWidth: 32, frameHeight: 48 }
+  );
+}
 
-function create() {}
+function create() {
+  this.add.image(400, 300, "sky");
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
+}
 
 function update() {}
